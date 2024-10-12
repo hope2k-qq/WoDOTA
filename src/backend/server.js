efdc7b414416ea1eb86dbc6b0dabdf3030270c3c
Антон Козакевич <teamglobalua@gmail.com>
2024-10-12 20:29:13 +0300

    Fix bug cors

--- src/backend/server.js
+++ src/backend/server.js
@@ -10,12 +10,11 @@ const apicache = require('apicache');
 const port = process.env.PORT || 5000;
 const cache = apicache.middleware;
 
-const corsOptions = {
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-};
-
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const replacements = {
     'arc_warden': 'roshan',
