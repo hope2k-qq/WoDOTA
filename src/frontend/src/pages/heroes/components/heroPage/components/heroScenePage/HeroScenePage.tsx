1e81115ddd814473e30add46fd7a56b4a69c8173
Антон Козакевич <teamglobalua@gmail.com>
2025-03-04 21:10:28 +0100

    fix #8 v3

--- src/frontend/src/pages/heroes/components/heroPage/components/heroScenePage/HeroScenePage.tsx
+++ src/frontend/src/pages/heroes/components/heroPage/components/heroScenePage/HeroScenePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 //import { HeroModelPage } from "./components/heroModelPage/HeroModelPage";
