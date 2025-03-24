1b0c94915b22e1babaaa739f5e8f33a380070c9e
Антон Козакевич <teamglobalua@gmail.com>
2025-03-24 13:35:49 +0100

    fix 429 error steam

--- src/backend/routes/tournamentsRoutes.js
+++ src/backend/routes/tournamentsRoutes.js
@@ -1,9 +1,11 @@
 ﻿const express = require('express');
 const router = express.Router();
 const tournamentsController = require('../controllers/tournamentsController');
+const apicache = require('apicache');
+const cache = apicache.middleware;
 
-router.get('/tournament/players', tournamentsController.getTournamentList);
-router.get('/tournament/qualifiers', tournamentsController.getTournamentQualifiers);
-router.get('/tournament/playoffs', tournamentsController.getTournamentPlayoffs);
+router.get('/tournament/players', cache('60 minutes'), tournamentsController.getTournamentList);
+router.get('/tournament/qualifiers', cache('60 minutes'), tournamentsController.getTournamentQualifiers);
+router.get('/tournament/playoffs', cache('60 minutes'), tournamentsController.getTournamentPlayoffs);
 
 module.exports = router
--- src/backend/services/playerService.js
+++ src/backend/services/playerService.js
@@ -1,44 +1,73 @@
 ﻿const axios = require('axios');
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const getPlayersInfoBySteamIds = async (friendshipCodes) => {
     const apiKey = process.env.STEAM_API_KEY;
     const playerInfo = {};
-
     const chunkSize = 50;
-    const promises = [];
+    const maxConcurrentRequests = 5;
+
+    const { default: pLimit } = await import('p-limit');
+    const limit = pLimit(maxConcurrentRequests);
 
     const steamIds = friendshipCodes.map(code => (BigInt(code) + BigInt(76561197960265728)).toString());
 
-
+    const chunks = [];
     for (let i = 0; i < steamIds.length; i += chunkSize) {
-        const chunk = steamIds.slice(i, i + chunkSize);
-        const steamIdsString = chunk.join(',');
+        chunks.push(steamIds.slice(i, i + chunkSize));
+    }
 
-        promises.push(
-            axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamIdsString}`)
-                .then(response => {
-                    if (response.data.response.players.length === 0) {
-                        return;
-                    }
-                    
+    const promises = [];
+    
+    const fetchWithRetry = async (url) => {
+        let retries = 5; 
+        let success = false;
 
-                    response.data.response.players.forEach(player => {
-                        const friendCode = (BigInt(player.steamid) - BigInt(76561197960265728)).toString();
-                        playerInfo[friendCode] = {
-                            avatar: player.avatarfull,
-                            personaName: player.personaname,
-                            profileUrl: player.profileurl,
-                        };
-                    });
-                })
-                .catch(error => {
+        while (!success && retries > 0) {
+            try {
+                const response = await axios.get(url);
+                
+                if (response.data.response.players.length > 0) {
+                    return response.data;
+                }
+                success = true; 
+            } catch (error) {
+                if (error.response && error.response.status === 429) {
+                    console.log('Rate limit exceeded, retrying...');
+                    await delay(1500);
+                    retries--;
+                } else {
                     console.error('Error fetching data from Steam API:', error.message);
-                })
-        );
+                    break;
+                }
+            }
+        }
+
+        return null;
+    };
+
+    for (let i = 0; i < chunks.length; i++) {
+        const chunk = chunks[i];
+        const steamIdsString = chunk.join(',');
+        
+        promises.push(limit(async () => {
+            const url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamIdsString}`;
+            const responseData = await fetchWithRetry(url);
+
+            if (responseData) {
+                responseData.response.players.forEach(player => {
+                    const friendCode = (BigInt(player.steamid) - BigInt(76561197960265728)).toString();
+                    playerInfo[friendCode] = {
+                        avatar: player.avatarfull,
+                        personaName: player.personaname,
+                        profileUrl: player.profileurl,
+                    };
+                });
+            }
+        }));
     }
 
     await Promise.all(promises);
-
     return playerInfo;
 };
 
